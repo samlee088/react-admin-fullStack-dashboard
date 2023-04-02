aa93a1ef9e67d75b54cdf120d01a82fdbb6ace93
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-04-01 18:48:59 -0700

    connection to mongo server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import morgan from 'morgan';
 import clientRoutes from './routes/client.js';
 import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
-import salesRoutes from './routes/routes.js';
+import salesRoutes from './routes/sales.js';
 
 
 
@@ -30,5 +30,12 @@ app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
 
-
+/* Mongoose Setup */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
 

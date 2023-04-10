8787540c5e3ab6a4ef6501f5e206d1b4a0500e03
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-04-09 17:23:15 -0700

    update with products page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { useMemo } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from 'scenes/dashboard';
 import Layout from 'scenes/layout';
+import Products from 'scenes/products';
+
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -21,6 +23,7 @@ function App() {
             <Route element={<Layout/>}>
               <Route path='/' element={<Navigate to='/dashboard' replace/>}/>
               <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/products' element={(<Products/>)}/>
             </Route>
           </Routes>
         </ThemeProvider>
